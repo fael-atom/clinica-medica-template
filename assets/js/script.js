b0ba9d62c09fd5ff17319218f8936766819f1877
Rafael Mota <rafaelmota.dev@gmail.com>
2023-03-14 18:30:26 -0300

    Corrige funcionalidade do menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-let menuHamburger = document.querySelector(".menu-mobile");
-let menuLista = document.querySelector('.menu nav ul');
-
 window.onload = () => {
-    menuHamburger.addEventListener("click", () => {
-    if (menuLista.style.display == 'flex'){
-      menuLista.style.display = 'none';
-    } else{
-      menuLista.style.display = 'flex';
+  let menuHamburger = document.querySelector(".menu-mobile");
+  let menuLista = document.querySelector(".menu nav ul");
+  
+  menuHamburger.addEventListener("click", function () {
+    if (menuLista.style.display == "flex") {
+      menuLista.style.display = "none";
+    } else {
+      menuLista.style.display = "flex";
     }
   });
 };
